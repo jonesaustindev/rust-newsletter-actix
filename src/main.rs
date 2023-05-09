14b352e4651a24a16db2d7452379d97eef6ae10c
Austin Jones <jonesaustindev@gmail.com>
2023-05-08 23:33:05 -0500

    arrange app and add db setup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::TcpListener;
+
+use rust_newsletter::{configuration, startup::run};
+
+#[tokio::main]
+async fn main() -> Result<(), std::io::Error> {
+    let configuration = configuration::get_configuration().expect("Failed to read configuration.");
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let listener = TcpListener::bind(address).expect("Failed to bind random port");
+    run(listener)?.await
 }
